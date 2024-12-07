bb8c9ec491a13f14d4868cee177f61b1e14e9b8e
tomsegura2 <105571254+tomsegura2@users.noreply.github.com>
2024-12-07 09:48:27 -0600

    Update main.go
    Updated to include current correct login fields and handling of CSRF tokens

--- blinkist/main.go
+++ blinkist/main.go
@@ -1,30 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
-	"os"
-	"strings"
 
 	"github.com/gocolly/colly"
 )
 
-// variables
-var baseURL = "https://www.blinkist.com"
-var filename = "books_urls.txt"
-var username = "xxxxx@xxxxxx.xx" // !!! change to your e-mail address
-var password = "xxxxxxx"         // !!! enter your password here
-
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+// User credentials
+var username = "username"
+var password = "password"
 
 func main() {
-	// links to 27 categories containing all of the books
-	categories := [27]string{
+	// Initialize Colly collector
+	c := colly.NewCollector(
+		colly.AllowedDomains("www.blinkist.com"),
+		colly.Async(true),
+	)
+
+	// Fetch CSRF token
+	var csrfToken string
+	c.OnHTML("input[name='authenticity_token']", func(e *colly.HTMLElement) {
+		csrfToken = e.Attr("value")
+		log.Println("Fetched CSRF Token:", csrfToken)
+	})
+
+	// Visit login page
+	err := c.Visit("https://www.blinkist.com/en/nc/login/")
+	if err != nil {
+		log.Fatal("Error visiting login page:", err)
+	}
+
+	// Perform login
+	err = c.Post("https://www.blinkist.com/en/nc/login/", map[string]string{
+		"login[email]":    username,
+		"login[password]": password,
+		"authenticity_token": csrfToken,
+	})
+	if err != nil {
+		log.Fatal("Login failed:", err)
+	}
+
+	// Attach response logging
+	c.OnResponse(func(r *colly.Response) {
+		log.Printf("Response received. Status Code: %d\n", r.StatusCode)
+	})
+
+	// Define categories
+	categories := []string{
 		"https://www.blinkist.com/en/nc/categories/entrepreneurship-and-small-business-en/books",
 		"https://www.blinkist.com/en/nc/categories/science-en/books",
 		"https://www.blinkist.com/en/nc/categories/economics-en/books",
@@ -53,82 +75,13 @@ func main() {
 		"https://www.blinkist.com/en/nc/categories/biography-and-memoir-en/books",
 		"https://www.blinkist.com/en/nc/categories/creativity-en/books",
 	}
-	// create a new collector
-	c := colly.NewCollector(
-		colly.MaxDepth(1),
-		colly.AllowedDomains("www.blinkist.com"),
-	)
 
-	// authenticate
-	err := c.Post("https://www.blinkist.com/en/nc/login/", map[string]string{"username": username, "password": password})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// attach callbacks after login
-	c.OnResponse(func(r *colly.Response) {
-		log.Print("response received HTTP", r.StatusCode)
-	})
-
-	// on every a element which has href attribute call callback
-	os.Create("temp")
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// if link starts with /en/books return from callback
-		if !strings.HasPrefix(link, baseURL+"/en/books") {
-			return
-		}
-		// print links to file
-		f, err := os.OpenFile("temp", os.O_APPEND|os.O_WRONLY, 0600)
-		if err != nil {
-			panic(err)
-		}
-
-		defer f.Close()
-
-		if _, err = f.WriteString(strings.Replace(link, "/en/books", "/en/nc/reader", -1) + "\n"); err != nil {
-			panic(err)
-		}
-	})
-
-	// start scraping
+	// Scrape each category
 	for _, url := range categories {
+		log.Println("Visiting category:", url)
 		c.Visit(url)
 	}
 
-	// start removing duplicate entries
-	os.Create(filename)
-	line, _ := ioutil.ReadFile("temp")
-	strLine := string(line)
-	lines := strings.Split(strLine, "\n")
-	resultSlice := removeDuplicates(lines)
-	f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Println(err)
-	}
-	for i := range resultSlice {
-		f.Write([]byte(resultSlice[i] + "\n"))
-	}
-	f.Close()
-	os.Remove("temp")
-}
-
-func removeDuplicates(s []string) []string {
-	counter := 0
-	m := make(map[string]bool)
-	for _, item := range s {
-		if _, ok := m[item]; ok {
-			// duplicate item
-			counter++
-			fmt.Println(item, "is a duplicate", "#", counter)
-		} else {
-			m[item] = true
-		}
-	}
-
-	var result []string
-	for item := range m {
-		result = append(result, item)
-	}
-	return result
+	// Wait for all async operations to complete
+	c.Wait()
 }
